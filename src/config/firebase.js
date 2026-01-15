ff98a64744fd0d1124b295ac1d9bd288c288e906
Shetty-Aryan <shetty147aryan@gmail.com>
2026-01-15 15:02:32 +0530

    Use env-based Firebase credentials for Render

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,16 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../../serviceAccountKey.json");
+
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT env variable not set");
+}
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "file-storage-d4382.firebasestorage.app"
+  storageBucket: "your-project-id.appspot.com"
 });
 
 const bucket = admin.storage().bucket();
